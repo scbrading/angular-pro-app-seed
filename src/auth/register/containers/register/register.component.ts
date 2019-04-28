cad125cad1a0b1c1bc1d32dd786d1ccf11a32a8f
Clark Brading <scbrading@gmail.com>
2019-04-28 16:18:37 -0400

    Set up Auth module.

---
+++ src/auth/register/containers/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  template: `
+    <div>Register
+    <auth-form></auth-form>
+    </div>
+  `,
+})
+export class RegisterComponent {}
